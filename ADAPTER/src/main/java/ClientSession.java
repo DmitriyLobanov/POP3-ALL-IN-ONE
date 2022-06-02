35e33f91c4a12f614140dce0693384e7a666899d
DmitriyLobanov <72847027+DmitriyLobanov@users.noreply.github.com>
2022-06-02 20:50:21 +0700

    Update ClientSession.java

--- ADAPTER/src/main/java/ClientSession.java
+++ ADAPTER/src/main/java/ClientSession.java
@@ -22,61 +22,87 @@ public class ClientSession {
     }
 
     public static void main(String[] args) {
-        FileUtility fileUtility = new FileUtility("C:\\Users\\guard\\Desktop\\demo\\JavaPOP3Client\\src\\toServerTransitionTour.txt");
+        FileUtility fileUtility = new FileUtility("C:\\Users\\guard\\Desktop\\demo\\JavaPOP3Client\\src\\toServerWp.txt");
         ArrayList<String> command = fileUtility.getCommandSequence();
         ArrayList<String> input = fileUtility.getInputDataToServerSequence();
         ArrayList<String> commandFromServer = new ArrayList<>();
         ArrayList<String> inputFromServer = new ArrayList<>();
         List<String> convertedListOutput = fileUtility.initHashMap(command);
+        ArrayList<String> testSeqAsStrings = fileUtility.getReadTestSequence();
+        String toServer;
 
+        String fromServer =null;
+        String commandToServer =null;
+        String commandArg =null;
         StringBuilder stringBuilder = null;
+        int index = 0;
+        in = null;
+        out = null;
+        int k = 0;
         try {
             socket = new Socket("localhost", 8089);
             reader = new BufferedReader(new InputStreamReader(System.in));
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//принять
             out = new PrintWriter(socket.getOutputStream(), true);
-
             System.out.println("Введите сообщение с клиента");
             System.out.println(in.readLine());
-            String toServer;
-            
-            String fromServer;
-            String commandToServer;
-            String commandArg;
-            stringBuilder = new StringBuilder();
-
-            for (int i = 0, j = 0; j < command.size(); ++i, ++j) {
-                if (command.get(j).contains("RES")) {
-                    command.remove(j);
-                    reset();
-                    in.readLine();
-                    stringBuilder.append("\n");
-                    j--;
-                    continue;
-                }
-
-                commandToServer = command.get(j);
-                commandArg = input.get(i);
-                toServer = commandToServer + " " + commandArg;
-                toServer = toServer.trim();
-                out.println(toServer);
-
-                if ((fromServer = in.readLine()).trim() == null) {
-                    inputFromServer.add("-ERR/" + " ");
-                    stringBuilder.append(commandToServer + "_" + convertedListOutput.get(i) + "/" + "-ERR" + " ");
-                    continue;
-                }
-                //fromServer = in.readLine().trim();
-                System.out.println(fromServer);
-                commandFromServer.add(commandToServer + " ");
-                List <String> arg =  List.of(fromServer.split("\\s"));
-                inputFromServer.add(arg.get(0) + "/" + " ");
-                stringBuilder.append(commandToServer + "_" + convertedListOutput.get(i) + "/" + arg.get(0) + " ");
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
-        finally {
+
+            /*System.out.println("Введите сообщение с клиента");
+            System.out.println(in.readLine());
+*/
+            stringBuilder = new StringBuilder();
+            int lineNumber = 0;
+            for (int i = 0, j = 0; j < command.size(); ++i, ++j) {
+                try {
+                    if (command.get(j).contains("RES")) {
+                        command.remove(j);
+                        reset();
+                        in.readLine();
+                        stringBuilder.append("\n");
+                        j--;
+                        ++index;
+                        ++lineNumber;
+                        continue;
+                    }
+
+                    commandToServer = command.get(j);
+                    commandArg = input.get(i);
+                    toServer = commandToServer + " " + commandArg;
+                    toServer = toServer.trim();
+                    ++index;
+                    out.println(toServer);
+
+                    if ((fromServer = in.readLine()) == null) {
+                        inputFromServer.add("-ERR/" + " ");
+                        stringBuilder.append(commandToServer + "_" + convertedListOutput.get(i) + "/" + "-ERR" + " ");
+                        continue;
+                    }
+                    //fromServer = in.readLine().trim();
+                    System.out.println(fromServer.trim());
+                    commandFromServer.add(commandToServer + " ");
+                    List<String> arg = List.of(fromServer.split("\\s"));
+                    inputFromServer.add(arg.get(0) + "/" + " ");
+                    stringBuilder.append(commandToServer + "_" + convertedListOutput.get(i) + "/" + arg.get(0) + " ");
+
+            } catch (IOException ex) {
+
+          //  for (int k = 0; j < command.size(); ++k) {
+
+                if (testSeqAsStrings.get(lineNumber).contains("QUIT") && (i < index) ) {
+                    commandFromServer.add(commandToServer + " ");
+                    //List <String> arg =  List.of(fromServer.split("\\s"));
+                    inputFromServer.add("-ERR" + "/" + " ");
+                    stringBuilder.append(commandToServer + "_" + convertedListOutput.get(i) + "/" + "-ERR" + " ");
+                }
+                k  = j;
+                //index = 0;
+               // break;
+           // }
+        }
+       /* finally {
             fileUtility.saveResponseLocal(stringBuilder.toString());
             fileUtility.compareInputAndOutputSeq(stringBuilder.toString());
             try {
@@ -84,6 +110,10 @@ public class ClientSession {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
+
+    }
+        fileUtility.saveResponseLocal(stringBuilder.toString());
+        fileUtility.compareInputAndOutputSeq(stringBuilder.toString());
     }
 }
